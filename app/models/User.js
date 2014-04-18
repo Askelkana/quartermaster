15f9c5dfdfe6fb7e93ab17bb450050ee2c23cd38
askelkana <askelkana@gmail.com>
2014-04-18 07:02:02 +0300

    format

--- app/models/User.js
+++ app/models/User.js
@@ -2,7 +2,7 @@
 (function () {
 	var mongoose = require('mongoose');
 	var User = mongoose.model('User', new mongoose.Schema({
-		_id:       mongoose.Schema.ObjectId,
+		_id:      mongoose.Schema.ObjectId,
 		email:    String,
 		name:     String,
 		password: String,
